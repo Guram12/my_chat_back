e4ede5b9d0cc1ba03e4439f85e1557634e24343a
Guram12 <guram.shanidze.33@gmail.com>
2024-05-22 15:56:26 +0400

    user  to user massaging is working

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserList, UserDetail, MessageList, RoomList, RoomDetail, RoomMessagesView, logout, CurrentUserDetail
+from .views import UserList, UserDetail, MessageList, RoomList, RoomDetail, RoomMessagesView, logout, CurrentUserDetail, UserToUserMessages
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
@@ -7,9 +7,9 @@ urlpatterns = [
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('users/me/', CurrentUserDetail.as_view(), name='user-detail'),
     path('messages/', MessageList.as_view(), name='message-list'),
+    path('messages/<str:username>/', UserToUserMessages.as_view(), name='user-to-user-messages'),
     path('rooms/', RoomList.as_view(), name='room-list'),
     path('rooms/<int:pk>/', RoomDetail.as_view(), name='room-detail'),
-    # path('rooms/<int:room_id>/messages/', RoomMessages.as_view(), name='room-messages'),
     path('login/', obtain_auth_token, name='login'),
     path('logout/', logout, name='logout'),
     path('rooms/<str:room_name>/messages/', RoomMessagesView.as_view(), name='room-messages'),
