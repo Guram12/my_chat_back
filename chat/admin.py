9134a39417c5c95e84d1a1765dedc5c2fdaccd98
Guram12 <guram.shanidze.33@gmail.com>
2024-05-22 17:45:30 +0400

    start making custom user

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Message , Room
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import Message , Room 
+
 
 admin.site.register(Message)
 admin.site.register(Room)
 
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
+#     fieldsets = UserAdmin.fieldsets + (
+#         (None, {'fields': ('bio', 'location')}),
+#     )
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         (None, {'fields': ('bio', 'location')}),
+#     )
+
+# admin.site.register(CustomUser, CustomUserAdmin)
